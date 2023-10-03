f03a78cfef75b28aff531bb303a04d7f8d59be53
os2023 <os2023@os2023>
2023-10-03 16:22:08 +0200

    feat: added sleep and pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,21 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]){
+    int p[2];
+    char buf[20];
+    pipe(p);
+
+    int pid = fork();
+    if (pid == 0){
+        write(p[1], "ping", 4);
+        printf("Thread id %d: received ping\n", getpid());
+    }
+    else {
+        wait(0);
+        read(p[0], buf, 4);
+        printf("Thread id %d: received pong\n", getpid());
+    }
+    exit(1);
+}
